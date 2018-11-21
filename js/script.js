581c44613f626944387f2057116aff2809892b06
ailbib <tilab.dayan@gmail.com>
2018-11-21 00:56:14 -0300

    funcao getCells

--- js/script.js
+++ js/script.js
@@ -5,14 +5,27 @@
 
 let jogo = (function () {
 	let _celulas = [];
+	let _navios  = [];
+
+	let comparar = function () {
+		console.log('eae gay');
+	}
+	
+	let getCelulas = function () {
+		let tbl = document.getElementById('tab-adversario');
+		_celulas = Array.from(tbl.getElementsByTagName('td')); //converte pois eh retornado HTMLCollection
+
+		_celulas.forEach((elem) => {
+			elem.addEventListener('click', comparar);
+		});
+	}
 
 	let iniciar = function () {
-
+		getCelulas();
 	}
 
 	return {
-		iniciar: iniciar;
+		iniciar: iniciar,
 	}
 })();
 jogo.iniciar();
-console.log(jogo._celulas);
