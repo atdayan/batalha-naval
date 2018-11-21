8e748a645b3462c3edfd537092ad814e770d5236
ailbib <tilab.dayan@gmail.com>
2018-11-20 23:20:12 -0300

    -a

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-function receberCelula(celula) {
-	
-}
+// gerar os navios e seus lugares aleatoriamente
+// deixar o jogador fazer sua jogada
+// comparar pra ver se acertou
+// apagar célula correspondente da tabela
+
+let jogo = (function () {
+	let _celulas = [];
+
+	let iniciar = function () {
+
+	}
+
+	return {
+		iniciar: iniciar;
+	}
+})();
+jogo.iniciar();
+console.log(jogo._celulas);
